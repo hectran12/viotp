af95f8bf591f6512291251f47b83605a4921d251
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-05-28 18:54:54 +0700

    Net

---
+++ Networks.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace viotp
+{
+   public enum Networks
+       {
+           MOBIFONE,
+           VINAPHONE,
+           VIETTEL,
+           VIETNAMOBILE,
+           ITELECOM,
+       }
+}
